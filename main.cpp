7a692d1b19d37c94366fec502726062f6c140f70
carsoncrum <carsoncrum07@gmail.com>
2024-12-12 19:17:52 +0000

    Coded, Fixed Code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,69 @@
-// Calculating Rectangle Properties
-
-
+// Calculating The Safest Driving Area
+#include <limits>
+using namespace std;
 #include <iostream>
 int main()
 {
-  char choice;
-  do {
-    double length, width;
-    std::cout << "Enter the length of the rectangle: ";
-    std::cin >> length;
-    std::cout << "Enter the width of the rectangle: ";
-    double perimeter = 2 * (length + width);
-    double area = length * width;
-    std::cout << "Perimeter: " << perimeter << std::endl;
-    std::cout << "Area: " << area << std::endl;
+  int northAcc, southAcc, eastAcc, westAcc, centralAcc;
 
-    std::cout << "Do you want to process another rectangle (y/n)? ";
-    std::cin >> choice;
-  } while (choice == 'y' || choice == 'Y');
-  return 0;
+    cout << "Enter the number of accidents in the North region: ";
+    cin >> northAcc;
+    while (northAcc < 0) {
+      cout << "Invalid input. Please enter a non-negative number: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> northAcc;
+    }
+    cout << "Enter the number of accidents in the South region: ";
+    cin >> southAcc;
+    while (southAcc < 0) {
+      cout << "Invalid input. Please enter a non-negative number: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> southAcc;
+    }
+    cout << "Enter the number of accidents in the East region: ";
+    cin >> eastAcc;
+    while (eastAcc < 0) {
+      cout << "Invalid input. Please enter a non-negative number: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> eastAcc;
+    }
+    cout << "Enter the number of accidents in the West region: ";
+    cin >> westAcc;
+    while (westAcc < 0) {
+      cout << "Invalid input. Please enter a non-negative number: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> westAcc;
+    }
+    cout << "Enter the number of accidents in the Central region: ";
+    cin >> centralAcc;
+    while (centralAcc < 0) {
+      cout << "Invalid input. Please enter a non-negative number: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> centralAcc;
+    }
+  
+    int minAcc = min(northAcc, min(southAcc, min(eastAcc, min(westAcc, centralAcc))));
+
+    cout << "The following region(s) had the fewest accidents:" << endl;
+    if (northAcc == minAcc) {
+      cout << "North" << endl;
+    }
+    if (southAcc == minAcc) {
+      cout << "South" << endl;
+    }
+    if (eastAcc == minAcc) {
+      cout << "East" << endl;
+    }
+    if (westAcc == minAcc) {
+      cout << "West" << endl;
+    }
+    if (centralAcc == minAcc) {
+      cout << "Central" << endl;
+    }
+    return 0;
 }
