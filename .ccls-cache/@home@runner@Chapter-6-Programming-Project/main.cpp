f29c91f46eaf24f003b80191322e417efe408d81
carsoncrum <carsoncrum07@gmail.com>
2024-12-12 19:27:43 +0000

    Coded, Fixed Code

--- .ccls-cache/@home@runner@Chapter-6-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-6-Programming-Project/main.cpp
@@ -1,69 +1,17 @@
-// Calculating The Safest Driving Area
-#include <limits>
-using namespace std;
+// Calculating Celcius from Fahrenheit
+
 #include <iostream>
-int main()
-{
-  int northAcc, southAcc, eastAcc, westAcc, centralAcc;
+#include <iomanip>
+using namespace std;
 
-    cout << "Enter the number of accidents in the North region: ";
-    cin >> northAcc;
-    while (northAcc < 0) {
-      cout << "Invalid input. Please enter a non-negative number: ";
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> northAcc;
-    }
-    cout << "Enter the number of accidents in the South region: ";
-    cin >> southAcc;
-    while (southAcc < 0) {
-      cout << "Invalid input. Please enter a non-negative number: ";
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> southAcc;
-    }
-    cout << "Enter the number of accidents in the East region: ";
-    cin >> eastAcc;
-    while (eastAcc < 0) {
-      cout << "Invalid input. Please enter a non-negative number: ";
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> eastAcc;
-    }
-    cout << "Enter the number of accidents in the West region: ";
-    cin >> westAcc;
-    while (westAcc < 0) {
-      cout << "Invalid input. Please enter a non-negative number: ";
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> westAcc;
-    }
-    cout << "Enter the number of accidents in the Central region: ";
-    cin >> centralAcc;
-    while (centralAcc < 0) {
-      cout << "Invalid input. Please enter a non-negative number: ";
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> centralAcc;
-    }
-  
-    int minAcc = min(northAcc, min(southAcc, min(eastAcc, min(westAcc, centralAcc))));
-
-    cout << "The following region(s) had the fewest accidents:" << endl;
-    if (northAcc == minAcc) {
-      cout << "North" << endl;
-    }
-    if (southAcc == minAcc) {
-      cout << "South" << endl;
-    }
-    if (eastAcc == minAcc) {
-      cout << "East" << endl;
-    }
-    if (westAcc == minAcc) {
-      cout << "West" << endl;
-    }
-    if (centralAcc == minAcc) {
-      cout << "Central" << endl;
-    }
-    return 0;
+double fahrenheitToCelsius(double fahrenheit) {
+  return 5.0 / 9.0 * (fahrenheit - 32.0);
+}
+int main() {
+  cout << "Fahrenheit" << setw(10) << "Celsius" << endl;
+  for (int fahrenheit = 0; fahrenheit <= 20; fahrenheit++) {
+    double celsius = fahrenheitToCelsius(fahrenheit);
+    cout << setw(10) << fahrenheit << setw(10) << fixed << setprecision(2) << celsius << endl;
+  }
+  return 0;
 }
