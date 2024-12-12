7876a750ec1c5d3bed46f81480b08e3284ccc2de
carsoncrum <carsoncrum07@gmail.com>
2024-12-12 19:07:38 +0000

    Coded, Fixed Code

--- .ccls-cache/@home@runner@Chapter-6-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-6-Programming-Project/main.cpp
@@ -1,3 +1,22 @@
-#include <iostream>
+// Calculating Rectangle Properties
 
-int main() { std::cout << "Hello World!\n"; }
+
+#include <iostream>
+int main()
+{
+  char choice;
+  do {
+    double length, width;
+    std::cout << "Enter the length of the rectangle: ";
+    std::cin >> length;
+    std::cout << "Enter the width of the rectangle: ";
+    double perimeter = 2 * (length + width);
+    double area = length * width;
+    std::cout << "Perimeter: " << perimeter << std::endl;
+    std::cout << "Area: " << area << std::endl;
+
+    std::cout << "Do you want to process another rectangle (y/n)? ";
+    std::cin >> choice;
+  } while (choice == 'y' || choice == 'Y');
+  return 0;
+}
